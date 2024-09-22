728586d965a43ad2e6982fbb6b7a4294e8114d3c
Andrezza Hamburgo de Freire <andrezzahamburgo@gmail.com>
2024-09-22 17:24:43 -0300

    initial commit

---
+++ src/test/java/me/dio/lab_dio_spring/LabDioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.lab_dio_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
